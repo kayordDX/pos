799b82a1e4a63c54843f285b7230c1a149fe73ec
Jaco Kok <kokjaco2@gmail.com>
2023-11-28 09:31:52 +0200

    Added Error Can now load outlets from client api that was generated.

---
+++ src/lib/components/Error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Alert, AlertDescription, AlertTitle, Card } from "@kayord/ui";
+	import { AlertTriangleIcon } from "lucide-svelte";
+
+	export let message: string | undefined = undefined;
+</script>
+
+<Alert variant="destructive">
+	<div class="flex gap-2">
+		<AlertTriangleIcon class="h-4 w-4" />
+		<AlertTitle>Error</AlertTitle>
+	</div>
+	<AlertDescription>{message ?? "An error occurred"}</AlertDescription>
+</Alert>
--- src/routes/setup/SetupForm.svelte
+++ src/routes/setup/SetupForm.svelte
@@ -1,43 +1,42 @@
 <script lang="ts">
-	import { Button, Form } from "@kayord/ui";
+	import { Button, Form, Loader } from "@kayord/ui";
 	import { outletSchema, type OutletSchema } from "./schema";
 	import type { SuperValidated } from "sveltekit-superforms";
 
-	const outlets = [
-		{
-			name: "Jessica's",
-			value: 1,
-		},
-		{
-			name: "Eagle Canyon",
-			value: 2,
-		},
-		{
-			name: "Ruimsig",
-			value: 3,
-		},
-	];
+	import { createOutletList, useOutletListQueryOptions } from "$lib/api";
+	import Error from "$lib/components/Error.svelte";
+
+	const query = createOutletList();
+	console.log($query.isPending);
 
 	export let form: SuperValidated<OutletSchema>;
 </script>
 
 <Form.Root method="POST" {form} schema={outletSchema} let:config>
-	<Form.Field {config} name="outletId" let:attrs>
-		<Form.Item>
-			{@const { value } = attrs.input}
-			<Form.Label>Outlet</Form.Label>
-			<Form.Select selected={{ value, label: outlets.find((i) => i.value === value)?.name }}>
-				<Form.SelectTrigger placeholder="Select outlet to link to device" />
-				<Form.SelectContent>
-					{#each outlets as outlet}
-						<Form.SelectItem value={outlet.value}>{outlet.name}</Form.SelectItem>
-					{/each}
-				</Form.SelectContent>
-			</Form.Select>
-			<Form.Description>The current outlet this device is linked to.</Form.Description>
-			<Form.Validation />
-		</Form.Item>
-	</Form.Field>
+	{#if $query.isPending}
+		<Loader />
+	{/if}
+	{#if $query.error}
+		<Error message={$query.error.reason} />
+	{/if}
+	{#if $query.isSuccess}
+		<Form.Field {config} name="outletId" let:attrs>
+			<Form.Item>
+				{@const { value } = attrs.input}
+				<Form.Label>Outlet</Form.Label>
+				<Form.Select selected={{ value, label: $query.data.find((i) => i.id === value)?.name }}>
+					<Form.SelectTrigger placeholder="Select outlet to link to device" />
+					<Form.SelectContent>
+						{#each $query.data as outlet}
+							<Form.SelectItem value={outlet.id}>{outlet.name}</Form.SelectItem>
+						{/each}
+					</Form.SelectContent>
+				</Form.Select>
+				<Form.Description>The current outlet this device is linked to.</Form.Description>
+				<Form.Validation />
+			</Form.Item>
+		</Form.Field>
+	{/if}
 	<Form.Button type="submit">Submit</Form.Button>
 	<Button variant="link" href="/">Cancel</Button>
 </Form.Root>
