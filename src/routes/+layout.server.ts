fbd3729300d027addf1205c7aa4f8e4e41b93697
Jaco Kok <kokjaco2@gmail.com>
2024-01-17 19:09:05 +0200

    Latest packages.

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,14 +3,14 @@ import { PUBLIC_API_URL } from "$env/static/public";
 import type { SalesPeriod } from "$lib/api";
 
 export const load = (async ({ locals }) => {
-	// let salesPeriod: SalesPeriod | undefined = undefined;
-	// if (locals.outlet) {
-	// 	const salesPeriodResult = await fetch(
-	// 		`${PUBLIC_API_URL}/SalesPeriod/${locals.outlet.outletId}`
-	// 	);
-	// 	if (salesPeriodResult.ok) {
-	// 		salesPeriod = (await salesPeriodResult.json()) as SalesPeriod;
-	// 	}
-	// }
-	return { user: locals.user, outlet: locals.outlet };
+	let salesPeriod: SalesPeriod | undefined = undefined;
+	if (locals.outlet) {
+		const salesPeriodResult = await fetch(
+			`${PUBLIC_API_URL}/SalesPeriod/${locals.outlet.outletId}`
+		);
+		if (salesPeriodResult.ok) {
+			salesPeriod = (await salesPeriodResult.json()) as SalesPeriod;
+		}
+	}
+	return { user: locals.user, outlet: locals.outlet, salesPeriod };
 }) satisfies LayoutServerLoad;
