3177005ca8893bed9c789ad19b7dbecdf16eaf3e
Jaco Kok <kokjaco2@gmail.com>
2024-01-30 07:49:32 +0200

    Latest versions and header nav

--- src/lib/components/Header/Header.svelte
+++ src/lib/components/Header/Header.svelte
@@ -4,12 +4,13 @@
 	import { user } from "$lib/stores/userStore";
 	import { Button } from "@kayord/ui";
 	import { session } from "$lib/stores/session";
+	import { goto } from "$app/navigation";
 </script>
 
 <div class="bg-secondary p-2 flex justify-between h-14 items-center border-b-2 border-secondary">
-	<div class="flex items-center">
+	<button class="flex items-center" on:click={() => goto("/")}>
 		<img src="/logo.svg" alt="kayord-logo" class="h-10" />ayord.Pos
-	</div>
+	</button>
 	<span></span>
 	<div class="flex gap-2">
 		<ThemeSwitch class="text-secondary-foreground" />
