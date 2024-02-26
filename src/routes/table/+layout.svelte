b7d2160195a271cba705a6331618aa637d711daf
Jaco Kok <kokjaco2@gmail.com>
2024-02-26 21:50:13 +0200

    Now calling all actions

--- src/routes/table/+layout.svelte
+++ src/routes/table/+layout.svelte
@@ -12,12 +12,12 @@
 
 <slot />
 <div class="w-full flex mb-2 items-center justify-center fixed bottom-0">
-	<div class="bg-secondary flex items-center p-1 gap-2 rounded-sm px-2">
+	<div class="bg-secondary flex items-center py-1 gap-2 rounded-sm px-2">
 		<a class="flex items-center text-muted-foreground" href={`/waiter`}
 			><HomeIcon class="w-4 h-4" />
 		</a>
 		<a
-			class="flex items-center text-muted-foreground rounded-sm p-1 px-2"
+			class="flex items-center text-muted-foreground rounded-sm p-1 px-3"
 			class:bg-black={menuActive}
 			class:!text-foreground={menuActive}
 			href={`/table/menu/${data.bookingId}`}
@@ -25,7 +25,7 @@
 			<MenuIcon class="w-4 h-4 mr-2" /> <span class="text-sm">Menu</span>
 		</a>
 		<a
-			class="flex items-center text-muted-foreground rounded-sm p-1 px-2"
+			class="flex items-center text-muted-foreground rounded-sm p-1 px-3"
 			class:bg-black={basketActive}
 			class:!text-foreground={basketActive}
 			href={`/table/basket/${data.bookingId}`}
@@ -34,7 +34,7 @@
 			<span class="text-sm">Basket</span>
 		</a>
 		<a
-			class="flex items-center text-muted-foreground rounded-sm p-1 px-2"
+			class="flex items-center text-muted-foreground rounded-sm p-1 px-3"
 			class:bg-black={billActive}
 			class:!text-foreground={billActive}
 			href={`/table/bill/${data.bookingId}`}
---
+++ src/routes/table/pay/[id]/PaymentDone.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	import { Avatar, Button, Card } from "@kayord/ui";
+	import { ShieldCheckIcon } from "lucide-svelte";
+
+	const id = $page.params.id;
+</script>
+
+<Card.Root class="m-8">
+	<Card.Header class="flex flex-row items-center gap-2">
+		<Avatar.Root class="animate-pulse">
+			<Avatar.Fallback><ShieldCheckIcon class="text-green-500" /></Avatar.Fallback>
+		</Avatar.Root>
+		<div>
+			<Card.Title>Payment Successful</Card.Title>
+			<Card.Description>Payment Successful</Card.Description>
+		</div>
+	</Card.Header>
+	<Card.Content></Card.Content>
+	<Card.Footer>
+		<Button variant="secondary" href={`/table/bill/${id}`} class="w-full">Return to Bill</Button>
+	</Card.Footer>
+</Card.Root>
