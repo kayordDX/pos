efcc07775f080c6bb18a7c3447632c084f977eb1
Jaco Kok <kokjaco2@gmail.com>
2024-03-10 22:56:13 +0200

    New guest page and navigating you based on role.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,35 @@
 <script lang="ts">
 	import { Button } from "@kayord/ui";
 	import { status } from "$lib/stores/status";
+	import { goto } from "$app/navigation";
+
+	if ($status.roles.length == 0 || ($status.roles.length == 1 && $status.roles.includes("Guest"))) {
+		goto("/guest");
+	} else if ($status.roles.length == 1) {
+		if ($status.roles.includes("Waiter")) {
+			goto("/waiter");
+		} else if ($status.roles.includes("Chef")) {
+			goto("/kitchen");
+		} else if ($status.roles.includes("Manager")) {
+			goto("/manager");
+		}
+	}
 </script>
 
 <div class="m-8 flex gap-4">
-	<div class="flex flex-col">
-		<Button href="/waiter">Waiter</Button>
-	</div>
-	<div class="flex flex-col">
-		<Button href="/manager">Manager</Button>
-	</div>
-	<div class="flex flex-col">
-		<Button href="/kitchen">Kitchen</Button>
-	</div>
+	{#if $status.roles.includes("Waiter")}
+		<div class="flex flex-col">
+			<Button href="/waiter">Waiter</Button>
+		</div>
+	{/if}
+	{#if $status.roles.includes("Chef")}
+		<div class="flex flex-col">
+			<Button href="/kitchen">Kitchen</Button>
+		</div>
+	{/if}
+	{#if $status.roles.includes("Manager")}
+		<div class="flex flex-col">
+			<Button href="/manager">Manager</Button>
+		</div>
+	{/if}
 </div>
-
-{JSON.stringify($status)};
---
+++ src/routes/guest/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Button, Card } from "@kayord/ui";
+	import { CalendarOffIcon } from "lucide-svelte";
+</script>
+
+<div class="m-2">
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>Welcome Guest</Card.Title>
+			<Card.Description>
+				You are currently a guest and do not have permissions to access any other pages
+			</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			<div class="flex flex-col items-start">
+				<img src="/logo.svg" alt="kayord-logo" class="h-28 hover:animate-pulse" />
+				<div class="text-muted-foreground mt-4 text-sm">
+					Once you have been assigned a role you can start using the system
+				</div>
+			</div>
+		</Card.Content>
+	</Card.Root>
+</div>
