a7da8eed424ad0aac51ea7bce2345edda75f623a
Jaco Kok <kokjaco2@gmail.com>
2024-03-17 17:06:28 +0200

    Added total to kitchen

--- src/routes/(salesPeriod)/(clockedIn)/table/basket/[id]/+page.svelte
+++ src/routes/(salesPeriod)/(clockedIn)/table/basket/[id]/+page.svelte
@@ -43,7 +43,11 @@
 	{#if ($query.data?.orderItems.length ?? 0) <= 0}
 		<EmptyBasket />
 	{:else}
-		<div class="flex justify-end mt-4 gap-2 w-full">
+		<div class="flex flex-col mt-4 gap-2 w-full items-end">
+			<div class="flex items-center gap-2">
+				<div class="text-muted-foreground">Total:</div>
+				<h2>R{$query.data?.total.toFixed(2)}</h2>
+			</div>
 			<Button on:click={sendAllToKitchen} class="w-full">
 				Send All to Kitchen <ChefHatIcon class="h-4 w-4 ml-2" />
 			</Button>
