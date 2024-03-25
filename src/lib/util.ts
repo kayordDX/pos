8dec1dab325c3212a827e622bfc6f6168ed181c6
Jaco Kok <kokjaco2@gmail.com>
2024-03-25 15:17:17 +0200

    New debounce function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -16,3 +16,25 @@ export const getInitials = (string: string) => {
 	}
 	return initials;
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => any>(
+	callback: T,
+	wait: number,
+	immediate?: boolean
+): (...args: Parameters<T>) => void {
+	let timeout: NodeJS.Timeout | undefined;
+
+	return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+		const later = () => {
+			timeout = undefined;
+			if (!immediate) callback.apply(this, args);
+		};
+
+		const callNow = immediate && timeout === undefined;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+
+		if (callNow) callback.apply(this, args);
+	};
+}
