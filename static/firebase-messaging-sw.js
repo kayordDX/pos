fba55a0a81248a8e9efc58facd805c50a0ba75b9
Jaco Kok <kokjaco2@gmail.com>
2024-04-21 23:23:16 +0200

    firebase messaging sw

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,1 +1,26 @@
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
 
+const firebaseConfig = {
+	apiKey: "AIzaSyAoHwOFNJ0_ag0Ly4YZzGzdW_n5_NjC2uE",
+	authDomain: "kayord-pos.firebaseapp.com",
+	projectId: "kayord-pos",
+	storageBucket: "kayord-pos.appspot.com",
+	messagingSenderId: "249122681101",
+	appId: "1:249122681101:web:3b0c9caccdee78e304c044",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+	console.log("Received background message ", payload);
+
+	// const notificationTitle = payload.notification.title;
+	// const notificationOptions = {
+	// 	body: payload.notification.body,
+	// 	// TODO: static icon later
+	// };
+
+	// self.registration.showNotification(notificationTitle, notificationOptions);
+});
