24395c670b1ab160059cb3eaeb563d1cd03a3d06
Jaco Kok <kokjaco2@gmail.com>
2024-06-03 23:29:11 +0200

    Latest packages and portal test.

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	import { Button } from "@kayord/ui";
+	import { Button, Dialog, Drawer, DropdownMenu } from "@kayord/ui";
+	let dialogOpen = false;
+	let drawerOpen = false;
 
 	const setNotification = () => {
 		// new Notification("Test", { body: "this is more text", icon: "/logo.svg" });
@@ -12,4 +14,44 @@
 	};
 </script>
 
-<Button on:click={setNotification}>Test</Button>
+<div class="flex gap-8 flex-col p-6">
+	<Button on:click={setNotification}>Notification Test</Button>
+
+	<DropdownMenu.Root>
+		<DropdownMenu.Trigger><Button>Portal Test</Button></DropdownMenu.Trigger>
+		<DropdownMenu.Content>
+			<DropdownMenu.Item on:click={() => (dialogOpen = true)}>Profile</DropdownMenu.Item>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
+
+	<Dialog.Root bind:open={dialogOpen}>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Are you sure absolutely sure?</Dialog.Title>
+				<Dialog.Description>
+					This action cannot be undone. This will permanently delete your account and remove your
+					data from our servers.
+				</Dialog.Description>
+			</Dialog.Header>
+			<Button
+				on:click={() => {
+					drawerOpen = true;
+					dialogOpen = false;
+				}}>Drawer</Button
+			>
+		</Dialog.Content>
+	</Dialog.Root>
+
+	<Drawer.Root bind:open={drawerOpen}>
+		<Drawer.Content>
+			<Drawer.Header>
+				<Drawer.Title>Are you sure absolutely sure?</Drawer.Title>
+				<Drawer.Description>This action cannot be undone.</Drawer.Description>
+			</Drawer.Header>
+			<Drawer.Footer>
+				<Button>Submit</Button>
+				<Drawer.Close>Cancel</Drawer.Close>
+			</Drawer.Footer>
+		</Drawer.Content>
+	</Drawer.Root>
+</div>
