fdd2b4c34e9602685a574de0075906d79994c001
Jaco Kok <kokjaco2@gmail.com>
2025-05-29 11:45:48 +0200

    New Colors

---
+++ src/lib/themes.ts
@@ -0,0 +1,11 @@
+export const THEMES = [
+	{
+		name: "Default",
+		value: "default",
+	},
+	{
+		name: "Kayord",
+		value: "kayord",
+	},
+];
+export type Theme = (typeof THEMES)[number];
--- src/routes/(pages)/test/+page.svelte
+++ src/routes/(pages)/test/+page.svelte
@@ -1,18 +1,60 @@
 <script lang="ts">
-	import { Button } from "@kayord/ui";
-	import Comp1 from "./Comp1.svelte";
-	import Comp2 from "./Comp2.svelte";
-	import LocalStorage from "./LocalStorage.svelte";
-	import NotificationsTest from "./NotificationsTest.svelte";
-	import { counter } from "./store.svelte";
+	import { THEMES } from "$lib/themes";
+	import { Select } from "@kayord/ui";
+	import { setTheme, theme } from "@kayord/ui/mode-watcher";
+
+	const label = $derived(THEMES.find((t) => t.value === theme.current)?.name ?? "Select a theme");
+
+	const colors = [
+		"accent-foreground",
+		"accent",
+		"background",
+		"border",
+		"card-foreground",
+		"card",
+		"destructive-foreground",
+		"destructive",
+		"foreground",
+		"input",
+		"muted-foreground",
+		"muted",
+		"popover-foreground",
+		"popover",
+		"primary-foreground",
+		"primary",
+		"ring",
+		"secondary-foreground",
+		"secondary",
+		"sidebar-accent-foreground",
+		"sidebar-accent",
+		"sidebar-background",
+		"sidebar-border",
+		"sidebar-foreground",
+		"sidebar-primary-foreground",
+		"sidebar-primary",
+		"sidebar-ring",
+		"sidebar",
+	];
 </script>
 
 <div class="flex gap-8 flex-col p-6">
-	<NotificationsTest />
-	<LocalStorage />
-	<Button onclick={() => (counter.count += 1)}>+ {counter.count}</Button>
-	<div>
-		<Comp1 />
-		<Comp2 />
-	</div>
+	<Select.Root type="single" bind:value={() => theme.current, (v) => setTheme(v ?? "default")}>
+		<Select.Trigger size="sm" class="w-32">
+			{label}
+		</Select.Trigger>
+		<Select.Content align="end">
+			{#each THEMES as theme (theme.value)}
+				<Select.Item value={theme.value} label={theme.name}>{theme.name}</Select.Item>
+			{/each}
+		</Select.Content>
+	</Select.Root>
+</div>
+
+<div class="flex items-start justify-start gap-2 flex-wrap p-2 rounded-md m-2">
+	{#each colors as color}
+		<div class="flex items-center gap-2">
+			<div style={"background-color: var(--color-" + color + ");"} class="size-7 rounded-md"></div>
+			<div>{color}</div>
+		</div>
+	{/each}
 </div>
