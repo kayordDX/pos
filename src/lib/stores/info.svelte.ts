81aa7ed0985911f1081cc7aaa938175bd3a62f53
Jaco Kok <kokjaco2@gmail.com>
2025-06-02 20:58:38 +0200

    Only single url for now. Will fix on cors

--- src/lib/stores/info.svelte.ts
+++ src/lib/stores/info.svelte.ts
@@ -1,12 +1,12 @@
 import { page } from "$app/state";
-import { PUBLIC_API_URL, PUBLIC_API_URL_KAYORD } from "$env/static/public";
+import { PUBLIC_API_URL } from "$env/static/public";
 
 class Info {
 	#isKayord: boolean = $derived(
 		page.url.hostname == "pos.kayord.com" || page.url.hostname == "localhost"
 	);
 
-	#kayordURL: string = $derived(this.#isKayord ? PUBLIC_API_URL_KAYORD : PUBLIC_API_URL);
+	#kayordURL: string = PUBLIC_API_URL;
 
 	isKayord() {
 		return this.#isKayord;
