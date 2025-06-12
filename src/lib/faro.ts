6d8c7c5a00055b5c2285f1653993430ba7cae158
Jaco Kok <kokjaco2@gmail.com>
2025-06-12 22:29:46 +0200

    Using public env vars.

--- src/lib/faro.ts
+++ src/lib/faro.ts
@@ -1,9 +1,10 @@
 import { initializeFaro } from "@grafana/faro-web-sdk";
 import { dev } from "$app/environment";
+import { PUBLIC_FARO_KEY, PUBLIC_FARO_URL } from "$env/static/public";
 
 export const faro = initializeFaro({
-	url: "https://alloy.kayord.com/collect",
-	apiKey: "kayord",
+	url: PUBLIC_FARO_URL,
+	apiKey: PUBLIC_FARO_KEY,
 	app: {
 		name: "pos",
 		environment: dev ? "dev" : "prod",
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,2 @@
-import { faro } from "$lib/faro";
-
 export const ssr = false;
 export const prerender = false;
