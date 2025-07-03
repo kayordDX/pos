23f50a11a61b58d544dbd3a77d2e4dbaff950a94
Jaco Kok <kokjaco2@gmail.com>
2025-07-03 13:09:42 +0200

    new delay function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -48,3 +48,7 @@ export const arrayUnique = <T>(array: Array<T>) => {
 	}
 	return a;
 };
+
+export const delay = (ms: number): Promise<void> => {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+};
