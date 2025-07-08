8dcd9be69e672c559123f434fc5e9fb0cdcd1a69
Jaco Kok <kokjaco2@gmail.com>
2025-07-08 22:31:12 +0200

    Input box on quantity

--- src/lib/components/Quantity/Quantity.svelte
+++ src/lib/components/Quantity/Quantity.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Button, Card } from "@kayord/ui";
+	import { Button, Card, Input } from "@kayord/ui";
 	import { MinusIcon, PlusIcon } from "@lucide/svelte";
 
 	interface Props {
@@ -20,7 +20,7 @@
 
 <div class="flex items-center justify-center">
 	<Card.Root class="w-44 p-1">
-		<div class="flex items-center w-full">
+		<div class="flex items-center w-full gap-2">
 			<Button
 				variant="outline"
 				size="icon"
@@ -32,7 +32,7 @@
 				<span class="sr-only">Decrease</span>
 			</Button>
 			<div class="flex-1 text-center">
-				<div class="text-3xl font-bold tracking-tighter">{value}</div>
+				<Input bind:value class="text-3xl font-bold tracking-tighter text-center" />
 			</div>
 			<Button variant="outline" size="icon" class="s-10 shrink-0 rounded-full" onclick={increment}>
 				<PlusIcon class="h-4 w-4" />
