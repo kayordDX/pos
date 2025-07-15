84919040bf3fc24dda1ab80a2a57ab5abe867487
Jaco Kok <kokjaco2@gmail.com>
2025-07-15 22:36:51 +0200

    Payment Types Chart added

---
+++ src/routes/(pages)/manager/admin/stats/paymentTypes/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { scaleBand } from "d3-scale";
+	import { Highlight, LineChart, type ChartContextValue } from "layerchart";
+	import TrendingUpIcon from "@lucide/svelte/icons/trending-up";
+	import { Chart, Card } from "@kayord/ui";
+	import { cubicInOut } from "svelte/easing";
+	import { curveNatural } from "d3-shape";
+
+	const chartData = [
+		{ payment: "Account", amount: 1600, average: 1600 },
+		{ payment: "Cash", amount: 4622.0, average: 157 },
+		{ payment: "Credit Card", amount: 9392, average: 339 },
+		{ payment: "Halo", amount: 41454, average: 311 },
+	];
+	const chartConfig = {
+		amount: { label: "Amount", color: "var(--chart-1)" },
+		average: { label: "Average", color: "var(--chart-3)" },
+	} satisfies Chart.ChartConfig;
+	let context = $state<ChartContextValue>();
+</script>
+
+<div class="m-2 flex flex-col items-center">
+	<Card.Root class="w-xl">
+		<Card.Header>
+			<Card.Title>Payment Types</Card.Title>
+			<Card.Description
+				>Sales Period Payment Type vs Average of last 5 Sales Periods</Card.Description
+			>
+		</Card.Header>
+		<Card.Content>
+			<Chart.Container config={chartConfig}>
+				<LineChart
+					data={chartData}
+					x="payment"
+					xScale={scaleBand()}
+					axis="x"
+					series={[
+						{
+							key: "amount",
+							label: "Desktop",
+							color: chartConfig.amount.color,
+						},
+						{
+							key: "average",
+							label: "Mobile",
+							color: chartConfig.average.color,
+							props: {
+								"stroke-dasharray": "6,4",
+								strokeWidth: 1,
+							},
+						},
+					]}
+					props={{
+						spline: { curve: curveNatural, motion: "tween", strokeWidth: 3 },
+						highlight: { points: { r: 4 } },
+					}}
+				>
+					{#snippet belowMarks()}
+						<Highlight area={{ class: "fill-muted" }} />
+					{/snippet}
+					{#snippet tooltip()}
+						<Chart.Tooltip hideLabel />
+					{/snippet}
+				</LineChart>
+			</Chart.Container>
+		</Card.Content>
+	</Card.Root>
+</div>
