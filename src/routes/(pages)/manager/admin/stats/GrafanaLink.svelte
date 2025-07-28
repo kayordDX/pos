de6f67301eaa24e42ef355853c9ad32bd49f04f6
Jaco Kok <kokjaco2@gmail.com>
2025-07-28 21:37:17 +0200

    Aviate Skydeck

--- src/routes/(pages)/manager/admin/stats/GrafanaLink.svelte
+++ src/routes/(pages)/manager/admin/stats/GrafanaLink.svelte
@@ -6,6 +6,6 @@
 	<a
 		class="text-xs"
 		href="https://grafana.aviatesoft.com/d/d5678e67-e1a1-4e0e-83ee-567d269f6ecc/aviate-skydeck"
-		target="_blank">grafana.aviatesoft.com</a
+		target="_blank">Aviate Skydeck</a
 	>
 </span>
