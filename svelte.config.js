19d377496ac7329b5b2eecd620ace25232ec2fe6
Jaco Kok <kokjaco2@gmail.com>
2025-09-28 15:18:35 +0200

    Latest and orval updated.

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		alias: {
 			$lib: "./src/lib",
@@ -9,6 +11,11 @@ const config = {
 		adapter: adapter({
 			fallback: "index.html",
 		}),
+	},
+	vitePlugin: {
+		inspector: {
+			showToggleButton: "never",
+		},
 	},
 };
 
